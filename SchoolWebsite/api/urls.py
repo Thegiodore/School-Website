3b8729cd59403e186eefb3d50c086b61c8aa358f
Thegiodore <thegiodore.samson@gmail.com>
2025-09-08 11:54:37 +0800

    fixed contact form api

--- SchoolWebsite/api/urls.py
+++ SchoolWebsite/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .contact_form.views import ContactFormViewSet # Note the corrected import path
+from .contactUs_form.views import ContactFormViewSet # Note the corrected import path
 
 router = DefaultRouter()
 router.register(r'contact', ContactFormViewSet, basename='contact')
